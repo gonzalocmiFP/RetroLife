dc36968c605f06a8dfcbac8f152277b172b28eec
Aguado04 <aguadoplazaalejandro@gmail.com>
2024-02-06 12:16:11 +0100

    Login y registro

---
+++ RetroLife/app/src/main/java/com/example/retrolife/Login.java
@@ -0,0 +1,27 @@
+package com.example.retrolife;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Button registro = findViewById(R.id.cambioRegistro);
+        registro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login.this, Registro.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
